5741d8b0cc50662019e309f32224a238f2d9e90d
Ishan Shrestha <ishan.shrestha356@gmail.com>
2022-09-22 08:20:37 +0545

    Create a custom user model using AbstractUser, create roles, override a save method to save the instance of user created according to role

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,18 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+
+class CustomUser(AbstractUser):
+    class Role(models.TextChoices):
+        ADMIN = "ADMIN", 'Admin'
+        TEACHER = "TEACHER", 'Teacher'
+        STUDENT = "STUDENT", 'Student'
+    
+    base_role = Role.ADMIN
+    role = models.CharField(max_length=20, choices=Role.choices)
+
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            self.role = self.base_role
+            return super().save(*args, **kwargs)
